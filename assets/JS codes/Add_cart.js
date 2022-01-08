d5f57eb1484c75509ae4e0d25365a8983086621d
Venkatakrishnan434 <venkatakrishnan434@gmail.com>
2022-01-08 20:33:22 +0530

    added new files

--- assets/JS codes/Add_cart.js
+++ assets/JS codes/Add_cart.js
@@ -1,9 +1,11 @@
 let str = "";
-function cart(){
+
+
+function cart() {
 
     let allDetails = JSON.parse(localStorage.getItem("Orders"));
-    for(i=0;i<allDetails.length;i++){
-        str+="<div id=\"cart\"><img class=\"htr\" id=\"product"+JSON.stringify(i+1)+"alt=\"cart\" src="+allDetails[i].link+"><br><p class=\"dtr1\">"+allDetails[i].product+"</p><br><p class=\"dtr2\">"+allDetails[i].price+"</p></div><br><br><br><br><br><br><br><br><br><br>"
+    for (i = 0; i < allDetails.length; i++) {
+        str += "<div id=\"cart\"><img class=\"htr\" id=\"product" + JSON.stringify(i + 1) + "alt=\"cart\" src=" + allDetails[i].link + "><br><p class=\"dtr1\">" + allDetails[i].product + "</p><br><p class=\"dtr2\">" + allDetails[i].Totalprice + "</p><br><p class=\"dtr3\">" + allDetails[i].numberQuantity + "</p><br><p class=\"dtr4\">" + allDetails[i].cost + "</p><br></div><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"
     }
     return str;
 }
--- assets/JS codes/list_menu.js
+++ assets/JS codes/list_menu.js
@@ -1,16 +1,25 @@
 let details = [];
-
+function onPageLoad() {
+    const getFromLs = JSON.parse(localStorage.getItem('Orders'));
+    if(getFromLs != null){
+        console.log('w');
+        details = getFromLs;
+        return details;
+    }
+}
+onPageLoad();
 function add(x,y,z){
-event.preventDefault();
-    let numberQuantity = parseFloat(prompt("Enter the number of Quantities"));
-    let price = z*numberQuantity;
-    let allDetails = {
-        numberQuantity : numberQuantity,
-        price : price,
-        link : y,
-        product : x
+    event.preventDefault();
+    const numberQuantity = parseInt(prompt("Enter the number of Quantities"));
+    const price = z*numberQuantity;
 
-    
+    const allDetails = {
+        numberQuantity : numberQuantity,
+        Totalprice : price,
+        link : y,
+        product : x,
+        cost : z
+
     }
     details.push(allDetails);
     localStorage.setItem("Orders" , JSON.stringify(details));
